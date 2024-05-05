dcee9375a399199e8a1d40945bc63bfbc77a1c7b
Alexandr Belyy <sashabelyy123@gmail.com>
2024-05-05 17:58:35 +0500

    add lines

--- video_processing.py
+++ video_processing.py
@@ -3,12 +3,17 @@ import numpy as np
 import datetime
 from ultralytics import YOLO
 import torch
+from id_line_annotator import IdLineAnnotator
+import supervision as sv 
 
 class VideoPlayer():
 
     def __init__(self, model = 'yolov8n.pt'):
         self.cap = cv2.VideoCapture() 
         self.video_time = None 
+
+        self.line_zones = dict()
+        self.points = []
 
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model = YOLO(model).to(device)
@@ -19,6 +24,29 @@ class VideoPlayer():
         self.cap = cv2.VideoCapture(path)
         self.start_video_time = start_time
 
+    def add_line(self,x1,y1,x2,y2,width,height):
+        self.add_point(x1,y1,width, height)
+        self.add_point(x2,y2,width, height)
+
+    def add_point(self,x,y,width,height):
+        orig_width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        orig_height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+        Sx = orig_width/width
+        Sy = orig_height/height
+
+        point = sv.Point(x,y)
+        point.x = round(Sx * point.x)
+        point.y = round(Sy * point.y)
+        
+        self.points.append(point)
+
+        if len(self.points) == 2:
+            lz = sv.LineZone(start=self.points[0], end=self.points[1])
+            new_id = len(self.line_zones)
+            self.line_zones[new_id] = lz
+            self.points.clear()
+
     def __display_time(self, frame):
         elapsed_miliseconds = self.cap.get(cv2.CAP_PROP_POS_MSEC)
         self.video_time = self.start_video_time + datetime.timedelta(milliseconds=elapsed_miliseconds)
@@ -26,6 +54,7 @@ class VideoPlayer():
         return frame 
     
     def __predict_frame(self, frame):
+        tracker = sv.ByteTrack()
         results = self.model.track(source=frame,
                                     persist = True, 
                                     verbose=False,
@@ -33,8 +62,28 @@ class VideoPlayer():
                                     classes = [2,3,5,7], #car,motorcycle, bus, truck
                                     tracker='bytetrack.yaml')
         frame = results[0].plot()
+
+        detections = sv.Detections.from_ultralytics(results[0])
+        detections = tracker.update_with_detections(detections)
+
+        self.__lines_count(detections)
+
         return frame
     
+    def __lines_count(self, detections:sv.Detections):
+        for line_id in self.line_zones.keys():
+            lz = self.line_zones[line_id]
+            crossed_in, crossed_out = lz.trigger(detections)
+            crossed = crossed_in | crossed_out
+
+            objects_id = detections[crossed].tracker_id
+            print(line_id, ":", objects_id)
+
+    def __display_lines(self,frame):
+        line_annotator = IdLineAnnotator(thickness=2, text_thickness=2, text_scale=1)
+        for line_id in self.line_zones.keys():
+            frame = line_annotator.annotate(frame=frame, line_counter = self.line_zones[line_id], id=line_id)
+        return frame 
 
     def generate_frames(self):
         while self.cap.isOpened():
@@ -44,14 +93,14 @@ class VideoPlayer():
             if not ret:
                 return 
             
-            frame = self.__predict_frame(frame)
             frame = self.__display_time(frame)
+            frame = self.__display_lines(frame)
+            frame = self.__predict_frame(frame)
             
-
             compression_level = 30
             buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY, compression_level])[1]
             frame = buffer.tobytes()
-        
+
             yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
    
