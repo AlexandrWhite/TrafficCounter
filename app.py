19b2392bf3a3941fd30f1f5d89b3524eab071e7f
Alexandr Belyy <sashabelyy123@gmail.com>
2024-05-06 19:04:14 +0500

    add full_playlist and remove debug

--- app.py
+++ app.py
@@ -20,12 +20,21 @@ playlist = [
     ('/content/drive/MyDrive/video1may3/1030to1100.mp4','2024-01-05 10:32:10'),
     ('/content/drive/MyDrive/video1may3/1100to1130.mp4','2024-01-05 10:58:47'),
     
-    #не грузит
-    ('/content/drive/MyDrive/video1may3/1130to1200.mp4','2024-01-05 10:58:47'),
+    #не грузил
+    ('/content/drive/MyDrive/video1may3/1130to1200.mp4','2024-01-05 11:29:14'),
 
     ('/content/drive/MyDrive/video1may3/1200to1230.mp4','2024-01-05 11:56:01'),
     ('/content/drive/MyDrive/video1may3/1230to1300.mp4','2024-01-05 12:26:05'),
     ('/content/drive/MyDrive/video1may3/1300to1330.mp4','2024-01-05 13:04:03'),
+    ('/content/drive/MyDrive/video1may3/1330to1400.mp4','2024-01-05 13:29:54'),
+    ('/content/drive/MyDrive/video1may3/1400to1430.mp4','2024-01-05 14:03:45'),
+    ('/content/drive/MyDrive/video1may3/1430to1500.mp4','2024-01-05 14:39:22'),
+    ('/content/drive/MyDrive/video1may3/1500to1530.mp4','2024-01-05 15:04:08'),
+    ('/content/drive/MyDrive/video1may3/1530to1600.mp4','2024-01-05 15:32:48'),
+    ('/content/drive/MyDrive/video1may3/1600to1630.mp4','2024-01-05 16:01:49'),
+    ('/content/drive/MyDrive/video1may3/1630to1700.mp4','2024-01-05 16:32:12'),
+    ('/content/drive/MyDrive/video1may3/1700to1730.mp4','2024-01-05 17:04:31'),
+    ('/content/drive/MyDrive/video1may3/1730to1800.mp4','2024-01-05 17:32:31'),
 ]   
 
 
@@ -50,4 +59,4 @@ def video():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
