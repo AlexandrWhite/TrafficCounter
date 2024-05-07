cb0bffe330ec0f99c3ee153e1e5114c3022947d4
Alexandr Belyy <sashabelyy123@gmail.com>
2024-05-07 08:49:29 +0500

    only frame

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ playlist = [
 vp.set_playlist(playlist)
 #vp.run_video(playlist[i][0], datetime.fromisoformat(playlist[i][1]))
 
+print("ONLY FRAME")
 vp.add_line(284,49, 617,253, 781,439)
 vp.add_line(161,170, 369,424, 781,439)
 vp.add_line(276,432, 607,212, 781,439)
