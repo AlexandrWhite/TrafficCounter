59ae6653db7384d964fb8c3335f5134a40cde2ec
Alexandr Belyy <sashabelyy123@gmail.com>
2024-05-07 09:21:41 +0500

    is alive

--- video_processing.py
+++ video_processing.py
@@ -146,7 +146,9 @@ class VideoPlayer():
             
             #minutes_of_video = self.cap.get(cv2.CAP_PROP_POS_MSEC)//1000
             #cv2.putText(frame,str(minutes_of_video//60)+":"+str(minutes_of_video%60),(25,25), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
-            
+
+            if frame_num % 10 == 0:
+                print(f'testing {frame_num}')            
 
             # if frame_num % 2000 == 0:
             #     print('CHECKPOINT')
