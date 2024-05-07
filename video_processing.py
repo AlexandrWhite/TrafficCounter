6dc93639ce811edcb9127efb963dfb9459e11057
Alexandr Belyy <sashabelyy123@gmail.com>
2024-05-07 14:21:14 +0500

    change to google drive path

--- video_processing.py
+++ video_processing.py
@@ -7,8 +7,8 @@ from id_line_annotator import IdLineAnnotator
 import supervision as sv 
 from progress.bar import IncrementalBar
 
-CSV_RESULT_PATH = 'dataset'
-#CSV_RESULT_PATH = '/content/drive/MyDrive/may1csv'
+#CSV_RESULT_PATH = 'dataset'
+CSV_RESULT_PATH = '/content/drive/MyDrive/may1csv'
 
 class VideoPlayer():
     class_to_str = {2:'car',3:'motorcycle',5:'bus',7:'truck'}
